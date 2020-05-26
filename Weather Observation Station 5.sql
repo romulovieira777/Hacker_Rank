e77a6063667ce37e073177f4a50d33c4ccac9470
romulovieira777 <61069548+romulovieira777@users.noreply.github.com>
2020-05-26 09:19:31 -0300

    Weather Observation Station 5
    Utilizando as palavras reservadas abaixo para solução do exercício.
    - Count
    - In
    - Length
    - Order By
    - Where

---
+++ Weather Observation Station 5.sql
@@ -0,0 +1,22 @@
+SELECT 
+    city, 
+    length_city
+FROM
+    (SELECT 
+        a.*, 
+        rownum r
+    FROM
+        (SELECT 
+			LENGTH(city) length_city, 
+            city
+    FROM
+        station
+    ORDER BY 
+		length_city , city) a)
+WHERE
+    r 
+IN
+	(1 , (SELECT 
+                COUNT(*)
+            FROM
+                station));
