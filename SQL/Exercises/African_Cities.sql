b1b27cfb74c61b755df03ee0b7fa2ea4957fa596
Romulo Vieira <romulo.vieira777@gmail.com>
2023-02-17 12:19:03 -0300

    SQL

--- SQL/Exercises/African_Cities.sql
+++ SQL/Exercises/African_Cities.sql
@@ -16,10 +16,10 @@ The CITY and COUNTRY tables are described as follows:
 Field        | Type           |
 -------------------------------
 ID           | NUMBER         |
-Name         | VARCHAR2(17)   |
-CountryCode  | VARCHAR2(3)    |
-District     | VARCHAR2(20)   |
-Population   | NUMBER         |
+NAME         | VARCHAR2(17)   |
+COUNTRYCODE  | VARCHAR2(3)    |
+DISTRICT     | VARCHAR2(20)   |
+POPULATION   | NUMBER         |
 -------------------------------
 
 
@@ -28,14 +28,14 @@ Population   | NUMBER         |
 -------------------------------
 Field          | Type         |
 -------------------------------
-Code           | VARCHAR2(3)  |
-Name           | VARCHAR2(44) |
-Continent      | VARCHAR2(13) |
-Region         | VARCHAR2(25) |
-SurfaceArea    | NUMBER       |
-IndepYear      | VARCHAR2(5)  |
-Population     | NUMBER       |
-LifeExpectancy | VARCHAR2(4)  |
+CODE           | VARCHAR2(3)  |
+NAME           | VARCHAR2(44) |
+CONTINENT      | VARCHAR2(13) |
+REGION         | VARCHAR2(25) |
+SURFACEAREA    | NUMBER       |
+INDEPYEAR      | VARCHAR2(5)  |
+POPULATION     | NUMBER       |
+LIFEEXPECTANCY | VARCHAR2(4)  |
 GNP            | NUMBER       |
 -------------------------------
 
